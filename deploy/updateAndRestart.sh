3d42e3eb1778b6bdca19ac67d90647596e9d877d
raaj2045 <raaj2045@gmail.com>
2020-01-28 22:22:59 +0530

    api ci/cd v0.6

--- deploy/updateAndRestart.sh
+++ deploy/updateAndRestart.sh
@@ -7,8 +7,8 @@ set -e
 rm -rf /home/ubuntu/bb-backend/
 
 # clone the repo again
-git config user.name "raaj2045"
-git config user.email "raaj2045@gmail.com"
+git --global config user.name "raaj2045"
+git --global config user.email "raaj2045@gmail.com"
 git clone https://gitlab.com/decoderslabs/bb-backend.git
 
 # stop the previous pm2
