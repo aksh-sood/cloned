a5cfa48fb4b84fad7207025291df49e4614a6d16
raaj2045 <raaj2045@gmail.com>
2020-01-29 07:56:15 +0530

    api ci/cd v0.95

--- deploy/updateAndRestart.sh
+++ deploy/updateAndRestart.sh
@@ -12,21 +12,21 @@ git config --global user.email "raaj2045@gmail.com"
 git clone https://gitlab+deploy-token-134290:miVzKRWirUzwo6kg8_od@gitlab.com/decoderslabs/bb-backend.git
 
 # stop the previous pm2
-pm2 kill
-npm remove pm2 -g
+sudo pm2 kill
+sudo npm remove pm2 -g
 
 
 #pm2 needs to be installed globally as we would be deleting the repo folder.
 # this needs to be done only once as a setup script.
-npm install pm2 -g
+sudo npm install pm2 -g
 # starting pm2 daemon
-pm2 status
+sudo pm2 status
 
 cd /home/ubuntu/bb-backend
 
 #install npm packages
 echo "Running npm install"
-npm install
+sudo npm install
 
 #Restart the node server
-npm run prod
+sudo npm run prod
